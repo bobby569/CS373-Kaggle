a8bfeda98fd99e53f9e385118374f65df9636b12
bobby569 <zhan2175@purdue.edu>
2017-11-16 12:44:56 -0500

    use 5 different model, achieve 0.826

--- Model_test/test.py
+++ Model_test/test.py
@@ -2,34 +2,46 @@ from Data import Data
 from preprocess import *
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier, ExtraTreeClassifier
-from sklearn.naive_bayes import GaussianNB, BernoulliNB, MultinomialNB
+from sklearn.naive_bayes import BernoulliNB
 from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier, \
                             BaggingClassifier, ExtraTreesClassifier, \
-                            GradientBoostingClassifier, VotingClassifier
-from sklearn.neural_network import MLPClassifier
-
-from sklearn.calibration import CalibratedClassifierCV
-from sklearn.linear_model import RidgeClassifier, RidgeClassifierCV, SGDClassifier
+                            GradientBoostingClassifier
+from sklearn.linear_model import RidgeClassifier
 from sklearn.neighbors import RadiusNeighborsClassifier, KNeighborsClassifier
 
+from sklearn.neural_network import MLPClassifier
 
 classifiers = [
-    DecisionTreeClassifier(max_depth=10),    # 0
-    GaussianNB(),
-    AdaBoostClassifier(),   # 2
+    DecisionTreeClassifier(max_depth=10, criterion="entropy"),      # 0.80
+    ExtraTreeClassifier(max_depth=13, criterion="entropy"),         # 0.69
+    BernoulliNB(alpha=5.5),                                         # 0.65
+    AdaBoostClassifier(learning_rate=1.2),                          # 0.79
+    RandomForestClassifier(max_depth=10, n_estimators=30,
+                           warm_start=True, bootstrap=False,
+                           criterion="entropy"),                    # 0.83
+    BaggingClassifier(n_estimators=40, warm_start=True),            # 0.95
+    ExtraTreesClassifier(max_depth=25, n_estimators=40,
+                         criterion="entropy"),                      # 0.78
+    GradientBoostingClassifier(learning_rate=0.4, n_estimators=110,
+                               max_depth=3, warm_start=True),       # 0.86
+    RidgeClassifier(),
+
+
     MLPClassifier(),
-    RandomForestClassifier(max_depth=10, n_estimators=20),
+
+
 ]
 
 d = Data()
 d.readTrainData()
 
-c = classifiers[-1]
+c = classifiers[8]
+iteration = 1
 
 total = 0
-for _ in range(10):
+for _ in range(iteration):
     X_train, X_test, y_train, y_test = train_test_split(d.attr, d.target, test_size=0.2)
     c.fit(X_train, y_train)
     total += c.score(X_test, y_test)
 
-print total
+print total / iteration
--- predict.py
+++ predict.py
@@ -1,22 +1,27 @@
 from Data import Data
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier, ExtraTreeClassifier
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier, \
+                            BaggingClassifier, ExtraTreesClassifier, \
+                            GradientBoostingClassifier
+from sklearn.linear_model import RidgeClassifier
+from sklearn.neighbors import RadiusNeighborsClassifier, KNeighborsClassifier
+
 from sklearn.neural_network import MLPClassifier
 
 classifiers = [
-    DecisionTreeClassifier(max_depth=10),
-    GaussianNB(),
-    AdaBoostClassifier(), # 2
-    MLPClassifier(activation="logistic", solver="sgd", learning_rate="adaptive"),
-    MLPClassifier(activation="tanh", solver="sgd", learning_rate="adaptive"),
-    MLPClassifier(activation="logistic", learning_rate="adaptive"),
-    MLPClassifier(activation="tanh", learning_rate="adaptive"),
-    RandomForestClassifier(max_depth=11, n_estimators=20),
+    DecisionTreeClassifier(max_depth=10, criterion="entropy"),
+    AdaBoostClassifier(learning_rate=1.2),
+    RandomForestClassifier(max_depth=10, n_estimators=30,
+                           warm_start=True, bootstrap=False,
+                           criterion="entropy"),
+    BaggingClassifier(n_estimators=40, warm_start=True),
+    GradientBoostingClassifier(learning_rate=0.4, n_estimators=110,
+                               max_depth=3, warm_start=True)
 ]
 
 trainData = Data()
 data = trainData.readTrainData()
 
-trainData.predict(classifiers[2], 'AdaBoost', '')
+trainData.predict(classifiers[4], 'GradientBoosting', '0.4_110')
